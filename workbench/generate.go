5648c2aaf0a649dc498f2ad08412a0773022adb3
Joe Corall <jjc223@lehigh.edu>
2026-07-08 16:04:48 -0400

    Add unpublished supplemental files column

--- workbench/generate.go
+++ workbench/generate.go
@@ -1,3 +1,3 @@
 package workbench
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config cfg.yaml ../workbench.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.5.1 -config cfg.yaml ../workbench.yaml
